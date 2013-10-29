6922714061e167a939d1e01ca0a4aadb863d1c22
zhushun0008 <zhushun0008@gmail.com>
2013-10-29 19:57:09 +0800

    Version 2.2 new file:   twoSparseAutoencoder.m

---
+++ twoSparseAutoencoder.m
@@ -0,0 +1,74 @@
+function [stack] = twoSparseAutoencoder(trainData,inputSize,hiddenSizeL1,hiddenSizeL2)
+%  change the code in this file. 
+%
+addpath ../sparseae_exercise/starter/
+addpath ../sparseae_exercise/starter/minFunc/
+addpath ../softmaxRegression/
+addpath ../selfTaught/
+
+%%======================================================================
+%% STEP 0: Initialize the parameters.
+sparsityParam = 0.1;   % desired average activation of the hidden units.
+                       % (This was denoted by the Greek alphabet rho, which looks like a lower-case "p",
+		               %  in the lecture notes). 
+lambda = 3e-3;         % weight decay parameter       
+beta = 3;              % weight of sparsity penalty term       
+
+
+%%======================================================================
+%% STEP 2: Train the first sparse autoencoder
+%  This trains the first sparse autoencoder on the unlabeled trainData.
+%  sae1OptTheta : Store the optimal W1, W2, B1, B2 
+
+%  Randomly initialize the parameters
+sae1Theta = initializeParameters(hiddenSizeL1, inputSize);
+options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
+                          % function. Generally, for minFunc to work, you
+                          % need a function pointer with two outputs: the
+                          % function value and the gradient. In our problem,
+                          % sparseAutoencoderCost.m satisfies this.
+options.maxIter = 400;	  % Maximum number of iterations of L-BFGS to run 
+options.display = 'on';
+
+[sae1OptTheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
+                                   inputSize, hiddenSizeL1, ...
+                                   lambda, sparsityParam, ...
+                                   beta, trainData), ...
+                              sae1Theta, options);
+
+%%======================================================================
+%% STEP 2: Train the second sparse autoencoder
+%  This trains the second sparse autoencoder on the first autoencoder
+%  features.
+%  sae2OptTheta : Store the optimal W1, W2, B1, B2 
+
+[sae1Features] = feedForwardAutoencoder(sae1OptTheta, hiddenSizeL1, ...
+                                        inputSize, trainData);
+
+%  Randomly initialize the parameters
+sae2Theta = initializeParameters(hiddenSizeL2, hiddenSizeL1);
+options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
+                          % function. Generally, for minFunc to work, you
+                          % need a function pointer with two outputs: the
+                          % function value and the gradient. In our problem,
+                          % sparseAutoencoderCost.m satisfies this.
+options.maxIter = 400;	  % Maximum number of iterations of L-BFGS to run 
+options.display = 'on';
+
+[sae2OptTheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
+                                   hiddenSizeL1, hiddenSizeL2, ...
+                                   lambda, sparsityParam, ...
+                                   beta, sae1Features), ...
+                              sae2Theta, options);
+
+%%======================================================================
+
+% Initialize the stack using the parameters learned
+stack = cell(2,1);
+stack{1}.w = reshape(sae1OptTheta(1:hiddenSizeL1*inputSize), ...
+                     hiddenSizeL1, inputSize);
+stack{1}.b = sae1OptTheta(2*hiddenSizeL1*inputSize+1:2*hiddenSizeL1*inputSize+hiddenSizeL1);
+stack{2}.w = reshape(sae2OptTheta(1:hiddenSizeL2*hiddenSizeL1), ...
+                     hiddenSizeL2, hiddenSizeL1);
+stack{2}.b = sae2OptTheta(2*hiddenSizeL2*hiddenSizeL1+1:2*hiddenSizeL2*hiddenSizeL1+hiddenSizeL2);
+end
